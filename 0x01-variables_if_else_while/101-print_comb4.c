6330106d2163422aba177ca1a8ff77069dcde144
djoobreel <50364936+djoobreel@users.noreply.github.com>
2022-03-11 21:44:37 -0800

    My 101pcmb4 com

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,21 +12,21 @@ int main()
 
 	for (i = 0; i <= 9; i++)
 		{
-			for (int j = i + 1; j <= 9; j++)
+			for (j = i + 1; j <= 9; j++)
 			{
-		    	        for (int k = j + 1; k <= 9; k++)
-			        {
+				for (k = j + 1; k <= 9; k++)
+				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
 					if (((i * 100) + (j * 10) + k) < 789)
 					{
-			    	                putchar(',');
+						putchar(',');
 						putchar(' '):
 					}
-		    	        }
+				}
 			}
 		}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
