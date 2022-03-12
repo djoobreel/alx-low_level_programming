5230f9614ff05b7ff7b61da159d1a4ce8fdca93e
djoobreel <50364936+djoobreel@users.noreply.github.com>
2022-03-11 21:40:51 -0800

    My 101pcmb4 com

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * Return: 0 on success. Error code otherwise
+ */
+
+int main()
+{
+	int a, b, c, i, j;
+
+	for (i = 0; i <= 9; i++)
+		{
+			for (int j = i + 1; j <= 9; j++)
+			{
+		    	        for (int k = j + 1; k <= 9; k++)
+			        {
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (((i * 100) + (j * 10) + k) < 789)
+					{
+			    	                putchar(',');
+						putchar(' '):
+					}
+		    	        }
+			}
+		}
+		putchar('\n');
+		return (0);
+}
