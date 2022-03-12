d47831bb33efaaaff7b0840db6a5343c96ce4b34
djoobreel <50364936+djoobreel@users.noreply.github.com>
2022-03-11 22:34:20 -0800

    My 102pcmb5 com

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,22 +18,22 @@ int main(void)
 				for (j = 48; j <= 57; j++)
 				{
 					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+						if (c + i + k + j == 227 && c == 57)
 						{
-							putchar(c);
-							putchar(i);
-							putchar(' ');
-							putchar(k);
-							putchar(j);
-							if (c + i + k + j == 227 && c == 57)
-							{
-								break;
-							}
-							else
-							{
-								putchar(',');
-								putchar(' ');
-							}
+							break;
 						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
 				}
 			}
 		}
